387efd78b16d6c6361223f11eff5cfd4e9287f81
3123824901 <3123824901@qq.com>
2017-08-10 10:29:59 +0800

    a

--- js/top.js
+++ js/top.js
@@ -1,14 +1,5 @@
 $(function(){
-	//用户名	
-	var str=Cookies.get('user');
-	var obj = str ? JSON.parse(str):{};
-	console.log(obj);
-	var username=obj.name1;
-	if(username!=""){
-		$("#top .top-l").find("span").text("Hi~~"+username+",欢迎回家!").next().hide();
-	}else{
-		$("#top .top-l").find("span").text("喵，欢迎来天猫").next().show();
-	}
+	
 	//购物车
 	var str2 = Cookies.get("cart");
 	var obj = str2 ? JSON.parse(str2) : {};
